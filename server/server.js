3a9078b5b586c17e84e73eda4a2324c79e8adcbd
Andrew Day <day_andy@hotmail.com>
2018-11-01 13:34:07 +1100

    Separated config to its own file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const config = require('./config/config');
 const express = require('express');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
@@ -8,7 +9,7 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = new express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json())
 
